94b33282093831cead946962e8ae15f2a95c730d
Kirubel12345678910 <6053472@mborijnland.nl>
2026-05-07 23:27:08 +0200

    header af

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="NL">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta author="Kirubel Birko">
+    <title>Header</title>
 </head>
 <body>
-    
+<header>
+    <section id="headertitel">
+    <article>
+    <h1>Stage lopen als  een Software Developer<h1>
+    <article>
+    </section>
+
+    <section id="headerfoto">
+        <img src="images/Schermafbeelding_2026-05-07_230510-removebg-preview(1).png" alt="foto van mijn gezicht">
+    </section>
+</header>
+
 </body>
 </html>
