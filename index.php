e5535fb4fe1e41f028eb48cd1490eda56f4e102f
Kirubel12345678910 <6053472@mborijnland.nl>
2026-05-07 21:45:47 +0200

    voorbereiding

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="NL">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta author="Kirubel Birko">
+    <link rel="stylesheet" href="css/style.css">
+    <title>About me</title>
 </head>
-<body>
-    
-</body>
+<?php
+include "includes/header.php";
+?>
+
+<?php
+include "includes/footer.php";
+?>
 </html>
